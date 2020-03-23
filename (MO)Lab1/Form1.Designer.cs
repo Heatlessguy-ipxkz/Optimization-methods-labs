8eaae812336499cb62015743bde339f85d446b85
Alex Kruddinger <ipxkz97@gmail.com>
2020-03-23 18:48:39 +0700

    Добавьте файлы проекта.

---
+++ (MO)Lab1/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace _MO_Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bClose = new System.Windows.Forms.Button();
+            this.bOk = new System.Windows.Forms.Button();
+            this.lDih = new System.Windows.Forms.Label();
+            this.lGold = new System.Windows.Forms.Label();
+            this.lSearch = new System.Windows.Forms.Label();
+            this.INewton = new System.Windows.Forms.Label();
+            this.IMiddlePoint = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bClose
+            // 
+            this.bClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.bClose.Location = new System.Drawing.Point(347, 130);
+            this.bClose.Name = "bClose";
+            this.bClose.Size = new System.Drawing.Size(75, 23);
+            this.bClose.TabIndex = 0;
+            this.bClose.Text = "Выход";
+            this.bClose.UseVisualStyleBackColor = true;
+            this.bClose.Click += new System.EventHandler(this.bClose_Click);
+            // 
+            // bOk
+            // 
+            this.bOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.bOk.Location = new System.Drawing.Point(347, 12);
+            this.bOk.Name = "bOk";
+            this.bOk.Size = new System.Drawing.Size(75, 112);
+            this.bOk.TabIndex = 2;
+            this.bOk.Text = "Расчитать";
+            this.bOk.UseVisualStyleBackColor = true;
+            this.bOk.Click += new System.EventHandler(this.bOk_Click);
+            // 
+            // lDih
+            // 
+            this.lDih.AutoSize = true;
+            this.lDih.Location = new System.Drawing.Point(12, 9);
+            this.lDih.Name = "lDih";
+            this.lDih.Size = new System.Drawing.Size(108, 13);
+            this.lDih.TabIndex = 3;
+            this.lDih.Text = "Метод дихотомии = ";
+            // 
+            // lGold
+            // 
+            this.lGold.AutoSize = true;
+            this.lGold.Location = new System.Drawing.Point(12, 53);
+            this.lGold.Name = "lGold";
+            this.lGold.Size = new System.Drawing.Size(141, 13);
+            this.lGold.TabIndex = 4;
+            this.lGold.Text = "Метод золотого сечения =";
+            // 
+            // lSearch
+            // 
+            this.lSearch.AutoSize = true;
+            this.lSearch.Location = new System.Drawing.Point(12, 31);
+            this.lSearch.Name = "lSearch";
+            this.lSearch.Size = new System.Drawing.Size(163, 13);
+            this.lSearch.TabIndex = 5;
+            this.lSearch.Text = "Метод равномерного поиска =";
+            // 
+            // INewton
+            // 
+            this.INewton.AutoSize = true;
+            this.INewton.Location = new System.Drawing.Point(12, 77);
+            this.INewton.Name = "INewton";
+            this.INewton.Size = new System.Drawing.Size(96, 13);
+            this.INewton.TabIndex = 6;
+            this.INewton.Text = "Метод Ньютона =";
+            // 
+            // IMiddlePoint
+            // 
+            this.IMiddlePoint.AutoSize = true;
+            this.IMiddlePoint.Location = new System.Drawing.Point(12, 102);
+            this.IMiddlePoint.Name = "IMiddlePoint";
+            this.IMiddlePoint.Size = new System.Drawing.Size(124, 13);
+            this.IMiddlePoint.TabIndex = 7;
+            this.IMiddlePoint.Text = "Метод средней точки =";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 175);
+            this.Controls.Add(this.IMiddlePoint);
+            this.Controls.Add(this.INewton);
+            this.Controls.Add(this.lSearch);
+            this.Controls.Add(this.lGold);
+            this.Controls.Add(this.lDih);
+            this.Controls.Add(this.bOk);
+            this.Controls.Add(this.bClose);
+            this.Name = "Form1";
+            this.Text = "Lab1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bClose;
+        private System.Windows.Forms.Button bOk;
+        private System.Windows.Forms.Label lDih;
+        private System.Windows.Forms.Label lGold;
+        private System.Windows.Forms.Label lSearch;
+        private System.Windows.Forms.Label INewton;
+        private System.Windows.Forms.Label IMiddlePoint;
+    }
+}
+
---
+++ (MO)Lab1/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace _MO_Lab1
+{
+    public partial class Form1 : Form
+    {
+        private int intervals = 10000;
+        private double step = 0.001;
+        private double XL = 0;
+        private double XR = 5;
+        private double Dih = 0, Gold = 0, Sea = 0, Mid = 0, Newt = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+
+        private double funct(double X, int proizv = 0)
+        {
+            //if (proizv == 1)
+            //{
+            //    return (X * X * X) + (2 * X) - 8;
+            //}
+            //    return (X * X * X * X) / 4 + (X * X) - 8 * X + 12;
+
+            //4x*x*x-36x2+94x-60 
+            if (proizv == 1)
+            {
+                return 4 * (X * X * X) - 36 * (X * X) + 94 * X - 60;
+            }
+            if (proizv == 2)
+            {
+                return 12 * (X * X) - 72 * X + 94;
+                //return Math.Pow(94, X) * Math.Log(94) + 12 * Math.Pow(X, 2) - 72 * X;
+            }
+            return X * X * X * X - 12 * (X * X * X) + 47 * (X * X) - 60 * X;
+        }
+
+
+
+        private void bOk_Click(object sender, EventArgs e)
+        {
+            Calculate();
+        }
+
+        private void Calculate()
+        {
+            lDih.Text = "Метод дихотомии = " + (Dih = Dichotomy(XL, XR, step));
+            lGold.Text = "Метод золотого сечения = " + (Gold = GoldenSection(XL, XR, step));
+            lSearch.Text = "Метод равномерного поиска = " + (Sea = Search(XL, XR, intervals));
+            INewton.Text = "Метод Ньютона = " + (Newt = Newton(XL, step));
+            IMiddlePoint.Text = "Метод средней точки = " + (Mid = middle_point(XL, XR, step));
+        }
+
+
+
+        private double Search(double a, double b, int iterations)
+        {
+            List<double> XArray = new List<double>();
+            List<double> YArray = new List<double>();
+            for (int i = 1; i < iterations; i++)
+            {
+                double temp = (a + (i * ((b - a) / (iterations + 1))));
+                XArray.Add(temp);
+                YArray.Add(funct(temp));
+            }
+
+            double answer = 10000;
+            int id = 0;
+
+            for(int i = 0; i < YArray.Count; i++)
+            {
+                if (YArray[i] < answer)
+                {
+                    answer = YArray[i];
+                    id = i;
+                }
+            }
+            return XArray[id];
+        }
+
+        private double Dichotomy(double a, double b, double pogr)
+        {
+            double y, z;
+            double eps = pogr * 0.2;
+            do
+            {
+                y = (a + b - eps) / 2;
+                z = (a + b + eps) / 2;
+                if (funct(y) <= funct(z))
+                    b = z;
+                else
+                    a = y;
+            }
+            while (Math.Abs(b - a) > pogr); 
+            return (a + b) / 2;
+        }
+
+        private double GoldenSection(double a, double b, double pogr)
+        {
+            double y = a + ((3 - Math.Sqrt(5)) / 2) * (b - a);
+            double z = a + b - y;
+
+            do
+            {
+                if (funct(y) <= funct(z))
+                {
+                    b = z;
+                    z = y;
+                    y = a + b - y;
+                }
+                else
+                {
+                    a = y;
+                    y = z;
+                    z = a + b - z;
+                }
+            }
+            while (Math.Abs(b - a) > pogr);
+            return (a + b) / 2;
+        }
+
+        private double Newton(double a, double pogr)
+        {
+            int iterations = 0;
+            double f1x;
+            do
+            {
+                f1x = funct(a, 1);
+                a -= f1x / funct(a, 2);
+                iterations++;
+            }  while (Math.Abs(f1x = funct(a, 1)) > pogr);
+            //Console.WriteLine("Newton iterations: {0}", iterations);
+            return a;
+        }
+
+        private double middle_point(double a, double b, double pogr)
+        {
+            int iterations = 0;
+            double z, f1z;
+            do
+            {
+                iterations++;
+                z = (a + b) / 2;
+                f1z = funct(z, 1);
+                if (f1z < 0)
+                    a = z;
+                else
+                    b = z;
+            } while (Math.Abs(f1z) > pogr);
+            //Console.WriteLine("Middle point iterations: {0}", iterations);
+            return z;
+        }
+        }
+}
